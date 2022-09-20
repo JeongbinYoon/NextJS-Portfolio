e281ed022e814216a58494c3702d217386c0cfa9
Jeongbin <yon980824@gmail.com>
2022-09-20 15:22:15 +0900

    Theme: Dark mode, Light mode

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import Layout from "../components/layout";
 
 export default function Projects() {
   return (
     <Layout>
+      <Head>
+        <title>윤정빈 포트폴리오</title>
+        <meta name="description" content="Next.JS 포트폴리오" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>프로젝트</h1>
     </Layout>
   );
