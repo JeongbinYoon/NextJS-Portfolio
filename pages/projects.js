b82f93aadd5b8e4f75a30c1c77ba5c1ecb3ba276
Jeongbin <yon980824@gmail.com>
2022-09-20 20:26:30 +0900

    Request API from Notion database

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import { DATABASE_ID, TOKEN } from "../config";
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  console.log(projects);
   return (
     <Layout>
       <Head>
@@ -9,7 +11,44 @@ export default function Projects() {
         <meta name="description" content="Next.JS 포트폴리오" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>프로젝트</h1>
+      <h1>총 프로젝트: {projects.length}</h1>
+
+      {projects.results.map((aProject) => (
+        <h1>{aProject.properties.Name.title[0].plain_text}</h1>
+      ))}
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const url = `https://api.notion.com/v1/databases/${DATABASE_ID}/query`;
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "Name",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(url, options);
+  const projects = await res.json();
+
+  // const projectNames = projects.results.map(
+  //   (aProject) => aProject.properties.Name.title[0].plain_text
+  // );
+
+  return {
+    props: { projects }, // will be passed to the page component as props
+  };
 }
