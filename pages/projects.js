e20fc715a087c733800ea7469d06d02f8048bc22
Jeongbin <yon980824@gmail.com>
2022-09-20 22:21:04 +0900

    Styled Projects component and applied the datas

--- pages/projects.js
+++ pages/projects.js
@@ -1,21 +1,29 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import ProjectItem from "../components/projects/project-item";
 import { DATABASE_ID, TOKEN } from "../config";
 
 export default function Projects({ projects }) {
-  console.log(projects);
+  // console.log(projects);
   return (
     <Layout>
-      <Head>
-        <title>윤정빈 포트폴리오</title>
-        <meta name="description" content="Next.JS 포트폴리오" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>총 프로젝트: {projects.length}</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen mb-10 px-6">
+        <Head>
+          <title>윤정빈 포트폴리오</title>
+          <meta name="description" content="Next.JS 포트폴리오" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트:
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
 
-      {projects.results.map((aProject) => (
-        <h1>{aProject.properties.Name.title[0].plain_text}</h1>
-      ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 lg:grid-cols-3">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
@@ -43,10 +51,6 @@ export async function getServerSideProps(context) {
 
   const res = await fetch(url, options);
   const projects = await res.json();
-
-  // const projectNames = projects.results.map(
-  //   (aProject) => aProject.properties.Name.title[0].plain_text
-  // );
 
   return {
     props: { projects }, // will be passed to the page component as props
